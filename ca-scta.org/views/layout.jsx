c9e3e894fcf98a6d823a57a1bc39d89e2e4906cd
Michael Billingham <mpbill@gmail.com>
2017-03-25 14:25:42 -0500

    sticky footer

--- ca-scta.org/views/layout.jsx
+++ ca-scta.org/views/layout.jsx
@@ -2,71 +2,8 @@
  * Created by mpbil on 3/24/2017.
  */
 let React = require('react');
-let classNames = require('classnames');
+let MainNav = require('./components/MainNav');
 
-
-
-class ActionLink extends React.Component{
-    render(){
-        let classes = classNames({'active':this.props.link==this.props.activeLink});
-        return <li className={classes}><a href={this.props.link}>{this.props.text}</a></li>;
-    }
-}
-class MainNav extends React.Component{
-    constructor(props){
-        super(props);
-        this.meetingListItemsMapper=this.meetingListItemsMapper.bind(this);
-    }
-    meetingListItemsMapper(m){
-        let linkPath = "/meetings/"+m.link;
-        return <li><ActionLink text={m.text} link={linkPath} activeLink={this.props.activeLink} /></li>;
-    }
-    render(){
-        let meetingListItems = this.props.meetingLinks.map(this.meetingListItemsMapper);
-
-
-        return (
-            <div  className="navbar navbar-inverse navbar-fixed-top">
-                <div  className="container-fluid">
-                    <div  className="navbar-header">
-                        <button type="button"  className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span  className="icon-bar" />
-                            <span  className="icon-bar" />
-                            <span  className="icon-bar" />
-                        </button>
-                        <a href="/" className="pull-left"><img src="http://content.ca-scta.org/Images/scta-logo.png"  className="navbar-brand ca-logo" /></a>
-
-                    </div>
-                    <div  className="navbar-collapse collapse">
-                        <ul  className="nav navbar-nav">
-                            <li  className="dropdown">
-                                <a href="#"  className="dropdown-toggle is-active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="False">Meetings</a>
-                                <ul  className="dropdown-menu">
-                                    {meetingListItems}
-                                </ul>
-                            </li>
-                            <ActionLink link="/contact" text="Contact" activeLink={this.props.activeLink}/>
-                            <ActionLink link="/twelveTraditions" text="12 Traditions" activeLink={this.props.activeLink}/>
-                            <ActionLink link="/areaResources" text="Area Resources" activeLink={this.props.activeLink}/>
-                            <ActionLink link="/events" text="Events" activeLink={this.props.activeLink}/>
-                            <ActionLink link="/flyers" text="Flyers" activeLink={this.props.activeLink}/>
-                            <ActionLink link="/hospitalsAndInstitutions" text="H&I" activeLink={this.props.activeLink}/>
-                            <ActionLink link="/newsletter" text="Newsletter" activeLink={this.props.activeLink}/>
-                            <li  className="dropdown">
-                                <a href="#"  className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="False">Conventions</a>
-                                <ul  className="dropdown-menu">
-                                    <ActionLink link="/worldConvention" text="World Convention" activeLink={this.props.activeLink}/>
-                                    <ActionLink link="/areaConvention" text="Area Convention" activeLink={this.props.activeLink}/>
-                                </ul>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
  class Layout extends React.Component {
     render() {
         return (
@@ -79,6 +16,7 @@ class MainNav extends React.Component{
             </head>
             <body>
             <MainNav meetingLinks={this.props.meetingLinks} activeLink={this.props.activeLink}/>
+            <div className="page-wrap">
             <div  className="container-fluid">
                 <div  className="row">
                     <div  className="col-lg-10 col-lg-offset-1 col-md-12">
@@ -88,8 +26,10 @@ class MainNav extends React.Component{
                     </div>
                 </div>
             </div>
-            <hr />
-            <footer>
+            </div>
+
+            <footer className="footer">
+                <hr />
                 <div  className="sixth-tradition">
                     <p>In the spirit of CA's 6th tradition, this website does not endorse any related facility or outside enterprise.</p>
                 </div>
